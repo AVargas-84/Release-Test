b08c719fdb67b601c299adbbc6a970ae19974fdb
Alejandro Vargas <vargas.yaver@gmail.com>
2025-03-03 19:07:09 -0500

    First Release

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Repo Release-Test",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Alejandro Vargas",                         # Tu nombre
+    author_email="vargas.yaver@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/AVargas-84/Release-Test",     # URL del proyecto
+)
